ee6116dd47e388e93f9dff6a7d1e052e04aaca55
ajao quyum <ajaoquyumolamide@gmail.com>
2025-07-14 09:05:03 +0100

    removed unused import

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,8 @@
-import React from 'react'
-import Header from '../components/Header';
-import Article1 from '../components/article1';
+import Article1 from '../components/Article1';
 import About from '../components/About';
 import AboutMore from '../components/AboutMore';
 import Services from '../components/Services';
 import Contact from '../components/Contact';
-import Footer from '../components/Footer';
 const HomePage = () => {
   return (
     <>
